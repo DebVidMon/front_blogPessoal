1074af2421227dc859a0b849f3838a0008ea113f
Debora <debvm@hotmail.com>
2021-04-04 19:33:51 -0300

    feat(usuario): atualizar usuário

--- src/app/edit/usuario-edit/usuario-edit.component.ts
+++ src/app/edit/usuario-edit/usuario-edit.component.ts
@@ -1,4 +1,8 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from './../../service/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/model/Usuario';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-usuario-edit',
@@ -6,10 +10,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario-edit.component.css']
 })
 export class UsuarioEditComponent implements OnInit {
+  user: Usuario = new Usuario()
+  idUser: number
+  confirmarSenha: string
+  tipo: string
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private aRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+    window.scroll(0, 0)
+
+    if (environment.token == '') {
+      this.router.navigate(['/entrar'])
+    }
+
+    this.idUser = this.aRoute.snapshot.params['id']
+    this.findByIdUser(this.idUser)
+  }
+
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUser(event: any) {
+    this.tipo = event.target.value
+  }
+
+  atualizar() {
+    this.user.tipoUsuario = this.tipo
+
+    if (this.user.senha != this.confirmarSenha) {
+      alert('A senhas estão incorretas.')
+    } else {
+      this.authService.cadastrar(this.user).subscribe((resp: Usuario) => {
+        this.user = resp
+        this.router.navigate(['/inicio'])
+        alert('Usuário atualizado com sucesso, faça o login novamente.')
+        environment.token = ''
+        environment.nome = ''
+        environment.foto = ''
+        environment.id = 0
+
+        this.router.navigate(['/entrar'])
+      })
+    }
+  }
+
+  findByIdUser(id: number) {
+    this.authService.getUserById(id).subscribe((resp: Usuario) => {
+      this.user = resp
+    })
   }
 
 }
